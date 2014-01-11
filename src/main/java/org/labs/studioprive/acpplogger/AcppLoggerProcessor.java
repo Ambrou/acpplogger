9ce55a4deb88c542044144799094329c2e90b2c0
apetitgenet <apetitgenet@free.fr>
2014-01-11 16:06:04 +0100

    Réussit à faire afficher une page de résultat

--- src/main/java/org/labs/studioprive/acpplogger/AcppLoggerProcessor.java
+++ src/main/java/org/labs/studioprive/acpplogger/AcppLoggerProcessor.java
@@ -3,15 +3,92 @@
  */
 package org.labs.studioprive.acpplogger;
 
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.AbstractBuild;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.labs.studioprive.acpplogger.service.AccpLoggerLog;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+
 /**
  * @author kdelfour
  * 
  */
 public class AcppLoggerProcessor {
 
-	public static void performParseLog(String fileToParse) {
-		// TODO Auto-generated method stub
+	private final String strFileToParse;
+	
+	private List<String> mListNameFile;
+	
+	public AcppLoggerProcessor(String strFileToParse)
+	{
+		this.strFileToParse = strFileToParse;
+		mListNameFile = new ArrayList<String>();	
+	}
+	
+	
+	public boolean performParseLog(AbstractBuild<?, ?> build, BuildListener listener) {
 		
+		final AccpLoggerLog accpLoggerLog = getAccpLoggerLogObject(listener);
+		FileWriter writer = null;
+		BufferedReader buff = null;
+		
+		try{
+			accpLoggerLog.infoConsoleLogger("start parsing file");
+			writer = new FileWriter(build.getWorkspace() + "\\ocR.html", true);
+			buff = new BufferedReader(new FileReader("C:\\Ambroise\\developpement\\Eclipse\\ocR.coverage"));
+			//buff = new BufferedReader(new FileReader(strFileToParse));
+			String line;
+			//accpLoggerLog.infoConsoleLogger("Read file");
+			accpLoggerLog.infoConsoleLogger("Write file" + build.getWorkspace() + "\\ocR.html");
+			while ((line = buff.readLine()) != null) {
+				writer.write(line,0,line.length());
+				writer.write("\n", 0, 1);
+			}
+		}
+		catch (IOException ioe) {
+			System.out.println("Erreur --" + ioe.toString());
+			accpLoggerLog.errorConsoleLogger(ioe.toString());
+		}
+		finally {
+			try {
+				if(buff != null) {
+					buff.close();
+				}
+				if(writer != null){
+					writer.close();
+				} 
+			}
+			catch (IOException ioe) {
+				accpLoggerLog.errorConsoleLogger(ioe.toString());
+			}
+		}
+		accpLoggerLog.infoConsoleLogger("stop parsing file");
+		return true;
+	}
+
+	private AccpLoggerLog getAccpLoggerLogObject(final BuildListener listener) {
+        return Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(BuildListener.class).toInstance(listener);
+            }
+        }).getInstance(AccpLoggerLog.class);
+    }
+
+
+	public String[] getResult() {
+		return mListNameFile.toArray(new String[mListNameFile.size()]);
 	}
 
 }
